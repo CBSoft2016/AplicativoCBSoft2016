a72391c1a020a044a50602794821c3f04ade305d
Vinicius Ribeiro Morais <vini.m.ribeiro@gmail.com>
2015-10-15 18:13:55 -0300

    Adicionando novos Packages

--- app/src/main/java/com/applications/fronchetti/cbsoft2016/Atividades/MainActivity.java
+++ app/src/main/java/com/applications/fronchetti/cbsoft2016/Atividades/MainActivity.java
@@ -1,7 +1,9 @@
-package com.applications.fronchetti.cbsoft2016;
+package com.applications.fronchetti.cbsoft2016.Atividades;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.applications.fronchetti.cbsoft2016.R;
 
 public class MainActivity extends AppCompatActivity {
 
