60b9bf1097b7e01681a14f9de6d98a5a4a9b6153
Vinicius Ribeiro Morais <vini.m.ribeiro@gmail.com>
2015-10-19 16:58:02 -0200

    Criação do Navigation Drawer

--- app/src/main/java/com/applications/fronchetti/cbsoft2016/Atividades/MainActivity.java
+++ app/src/main/java/com/applications/fronchetti/cbsoft2016/Atividades/MainActivity.java
@@ -2,14 +2,122 @@ package com.applications.fronchetti.cbsoft2016.Atividades;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.applications.fronchetti.cbsoft2016.R;
+import com.mikepenz.fontawesome_typeface_library.FontAwesome;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SectionDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 
 public class MainActivity extends AppCompatActivity {
+
+    //save our header or result
+    private AccountHeader headerResult = null;
+    private Drawer result = null;
+
+    private IProfile profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Handle Toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.titulo_cbsoft);
+
+        // Fazer acesso com a conta do Google!!
+        profile = new ProfileDrawerItem().withName("Nome do usuário").withEmail("usuario@gmail.com");
+
+        // Create the AccountHeader
+        buildHeader(false, savedInstanceState);
+
+        //Create the drawer
+        result = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withAccountHeader(headerResult) //set the AccountHeader we created earlier for the header
+                .addDrawerItems(
+                        new PrimaryDrawerItem().withName(R.string.fragmento_palestrante).withIcon(FontAwesome.Icon.faw_home),
+                        new SectionDrawerItem().withName(R.string.secao_cidade),
+                        new SecondaryDrawerItem().withName(R.string.fragmento_hotel_hostel).withIcon(FontAwesome.Icon.faw_bed),
+                        new SecondaryDrawerItem().withName(R.string.fragmento_onibus).withIcon(FontAwesome.Icon.faw_bus),
+                        new SecondaryDrawerItem().withName(R.string.fragmento_taxi).withIcon(FontAwesome.Icon.faw_taxi),
+                        new SecondaryDrawerItem().withName(R.string.fragmento_restaurante).withIcon(FontAwesome.Icon.faw_cutlery),
+                        new SecondaryDrawerItem().withName(R.string.fragmento_pontos_turisticos).withIcon(FontAwesome.Icon.faw_map_signs)
+                ) // add the items we want to use with our Drawer
+                .withOnDrawerNavigationListener(new Drawer.OnDrawerNavigationListener() {
+                    @Override
+                    public boolean onNavigationClickListener(View clickedView) {
+                        //this method is only called if the Arrow icon is shown. The hamburger is automatically managed by the MaterialDrawer
+                        //if the back arrow is shown. close the activity
+                        MainActivity.this.finish();
+                        //return true if we have consumed the event
+                        return true;
+                    }
+                })
+                .addStickyDrawerItems(
+                        new SecondaryDrawerItem().withName("About").withIcon(FontAwesome.Icon.faw_cog).withIdentifier(10)
+                )
+                .withSavedInstance(savedInstanceState)
+                .build();
     }
+
+    private void buildHeader(boolean compact, Bundle savedInstanceState) {
+        // Create the AccountHeader
+        headerResult = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withAlternativeProfileHeaderSwitching(false)
+                .withSelectionListEnabled(false)
+                .withHeaderBackground(R.drawable.header)
+                .withCompactStyle(compact)
+                .addProfiles(
+                        profile
+                ).withSavedInstance(savedInstanceState)
+                .build();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return true;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        //add the values which need to be saved from the drawer to the bundle
+        outState = result.saveInstanceState(outState);
+        //add the values which need to be saved from the accountHeader to the bundle
+        outState = headerResult.saveInstanceState(outState);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onBackPressed() {
+        //handle the back press :D close the drawer first and if the drawer is closed close the activity
+        if (result != null && result.isDrawerOpen()) {
+            result.closeDrawer();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
 }
