b5efb1f2ea94e5715b587a3b810da6c0a827da58
Felipe Fronchetti <fronchettiemail@gmail.com>
2015-11-25 14:53:49 -0200

    "Adicionando novas funcionalidades"

--- app/src/main/java/com/applications/fronchetti/cbsoft2016/Fragmentos/Taxis.java
+++ app/src/main/java/com/applications/fronchetti/cbsoft2016/Fragmentos/Taxis.java
@@ -1,10 +1,15 @@
 package com.applications.fronchetti.cbsoft2016.Fragmentos;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.applications.fronchetti.cbsoft2016.Adapters.Taxi;
